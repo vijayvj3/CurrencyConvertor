3b64e56d7745939a662c5ee2eaf4e9137a59c683
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-10-11 12:54:34 +0530

    Update junitTest.java

--- src/com/wipro/webappp/junitTest.java
+++ src/com/wipro/webappp/junitTest.java
@@ -1,15 +1,4 @@
 package com.wipro.webappp;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
-import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
-import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
- 
-import org.junit.Before;
-import org.junit.Test;
- 
-import net.sourceforge.jwebunit.util.TestingEngineRegistry;
  
  
 public class JWebUnitTest {
